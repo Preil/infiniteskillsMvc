d690115a9b81d378f731ac1ecfd70cc441e6c516
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-08-06 16:23:02 +0300

    URI template example. Project.jsp added. New controller method with PathVariable added.

--- src/main/java/com/infiniteskills/mvc/controllers/ProjectController.java
+++ src/main/java/com/infiniteskills/mvc/controllers/ProjectController.java
@@ -4,6 +4,7 @@ import com.infiniteskills.mvc.data.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,6 +18,12 @@ public class ProjectController {
 
     @Autowired
     private ProjectService projectService;
+
+    @RequestMapping(value = "/{projectId}")
+    public String findProject(Model model, @PathVariable("projectId") Long projectId){
+        model.addAttribute("project", projectService.find(projectId));
+        return "project";
+    }
 
     @RequestMapping(value = "/find")
     public String find(Model model){
