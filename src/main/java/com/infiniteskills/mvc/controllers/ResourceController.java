ce05e6f0200c3ce2096ff9592b712453f6e557b3
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-08-20 13:05:35 +0300

    Resource added as @ModelAttribute and as SessionAttribute.

--- src/main/java/com/infiniteskills/mvc/controllers/ResourceController.java
+++ src/main/java/com/infiniteskills/mvc/controllers/ResourceController.java
@@ -2,9 +2,9 @@ package com.infiniteskills.mvc.controllers;
 
 import com.infiniteskills.mvc.data.entities.Resource;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -16,13 +16,29 @@ import java.util.List;
 
 @Controller
 @RequestMapping(value = "/resource")
+@SessionAttributes("resource")
 public class ResourceController {
 
     @RequestMapping(value = "/add")
-    private String add(Model model){
-        model.addAttribute("resource", new Resource());
-        System.out.println("Add resource invoked");
+    private String add(){
         return "resource_add";
+    }
+
+    @RequestMapping("/review")
+    private String review(@ModelAttribute Resource resource){
+        System.out.println("Invoking review");
+        return "resource_review";
+    }
+
+    @RequestMapping(value = "/save")
+    private String save(@ModelAttribute Resource resource){
+        System.out.println(resource);
+        return "home";
+    }
+
+    @ModelAttribute("resource")
+    public Resource getResource(){
+        return new Resource();
     }
 
     @ModelAttribute("typeOptions")
@@ -38,12 +54,6 @@ public class ResourceController {
     @ModelAttribute("radioOptions")
     public List<String> getRadios(){
         return new LinkedList<>(Arrays.asList("Hours", "Pieces", "Tons"));
-    }
-
-    @RequestMapping(value = "/save")
-    private String save(@ModelAttribute Resource resource){
-        System.out.println(resource);
-        return "home";
     }
 
 
