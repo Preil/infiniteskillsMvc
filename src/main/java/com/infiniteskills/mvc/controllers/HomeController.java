fe3bca70213fd0502360521ebf925b32ee5aee79
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-08-29 07:27:52 +0300

    HomeController return value changed to "welcome".

--- src/main/java/com/infiniteskills/mvc/controllers/HomeController.java
+++ src/main/java/com/infiniteskills/mvc/controllers/HomeController.java
@@ -20,6 +20,6 @@ public class HomeController {
         project.setDescription("Simple project sponsored by NASA");
         model.addAttribute("currentProject", project);
 
-        return "home";
+        return "welcome";
     }
 }
