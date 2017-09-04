7feaf799d1be685b3ed09703e8b2fd7210f91e0c
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-09-04 12:56:34 +0300

    HomeController new method with request parameter added.

--- src/main/java/com/infiniteskills/mvc/controllers/HomeController.java
+++ src/main/java/com/infiniteskills/mvc/controllers/HomeController.java
@@ -2,15 +2,27 @@ package com.infiniteskills.mvc.controllers;
 
 import com.infiniteskills.mvc.data.entities.Project;
 import com.infiniteskills.mvc.data.entities.Sponsor;
+import com.infiniteskills.mvc.data.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Ilya 26.07.2017.
  */
 @Controller
 public class HomeController {
+
+    @Autowired
+    ProjectService service;
+
+    @RequestMapping(value="/", params = "projectId")
+    public String goHomeAgain(Model model, @RequestParam("projectId") Long projectId){
+        model.addAttribute("currentProject", this.service.find(projectId));
+        return "home";
+    }
 
     @RequestMapping("/")
     public String goHome(Model model) {
@@ -20,6 +32,6 @@ public class HomeController {
         project.setDescription("Simple project sponsored by NASA");
         model.addAttribute("currentProject", project);
 
-        return "welcome";
+        return "home";
     }
 }
