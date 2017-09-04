7f362092e9d4bf647f88978b6179e52877cf87c5
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-09-04 14:38:28 +0300

    Using FlashAttribute example.

--- src/main/java/com/infiniteskills/mvc/controllers/HomeController.java
+++ src/main/java/com/infiniteskills/mvc/controllers/HomeController.java
@@ -1,13 +1,12 @@
 package com.infiniteskills.mvc.controllers;
 
 import com.infiniteskills.mvc.data.entities.Project;
-import com.infiniteskills.mvc.data.entities.Sponsor;
 import com.infiniteskills.mvc.data.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Ilya 26.07.2017.
@@ -18,13 +17,13 @@ public class HomeController {
     @Autowired
     ProjectService service;
 
-    @RequestMapping(value="/", params = "projectId")
-    public String goHomeAgain(Model model, @RequestParam("projectId") Long projectId){
-        model.addAttribute("currentProject", this.service.find(projectId));
+    @RequestMapping(value="/")
+    public String goHomeAgain(Model model, @ModelAttribute("project") Project project){
+        model.addAttribute("currentProject", project);
         return "home";
     }
 
-    @RequestMapping("/")
+    /*@RequestMapping("/")
     public String goHome(Model model) {
         Project project = new Project();
         project.setName("First project");
@@ -33,5 +32,5 @@ public class HomeController {
         model.addAttribute("currentProject", project);
 
         return "home";
-    }
+    }*/
 }
