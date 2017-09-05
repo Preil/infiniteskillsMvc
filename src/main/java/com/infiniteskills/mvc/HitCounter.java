28ca936eae11b6eb26b0a5b321a4d535ad6b2fb6
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-09-05 18:04:03 +0300

    Class HitCounter added.

---
+++ src/main/java/com/infiniteskills/mvc/HitCounter.java
@@ -0,0 +1,16 @@
+package com.infiniteskills.mvc;
+
+/**
+ * Ilya 05.09.2017.
+ */
+public class HitCounter {
+    private int hits;
+
+    public int getHits() {
+        return hits;
+    }
+
+    public void setHits(int hits) {
+        this.hits = hits;
+    }
+}
