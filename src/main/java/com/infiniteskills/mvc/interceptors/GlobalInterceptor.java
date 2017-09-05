586e05cff1ab1f280352a90a4f143338a76474a2
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-09-05 18:15:58 +0300

    HitCounter added to GlobalInterceptor.

--- src/main/java/com/infiniteskills/mvc/interceptors/GlobalInterceptor.java
+++ src/main/java/com/infiniteskills/mvc/interceptors/GlobalInterceptor.java
@@ -1,5 +1,7 @@
 package com.infiniteskills.mvc.interceptors;
 
+import com.infiniteskills.mvc.HitCounter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,8 +12,14 @@ import java.util.Date;
  * Ilya 04.09.2017.
  */
 public class GlobalInterceptor extends HandlerInterceptorAdapter {
+
+    @Autowired
+    public HitCounter counter;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        counter.setHits(counter.getHits() + 1);
+        System.out.println("Hits: " + counter.getHits());
         request.setAttribute("currentDate", new Date());
         return super.preHandle(request, response, handler);
     }
